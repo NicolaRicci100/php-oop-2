c73fa88ba5b6e21a5d0e1c6632adc27983e1cb29
NicolaRicci100 <nicola.ricci09@gmail.com>
2023-07-24 16:25:06 +0200

    aggiunti dei prodotti di esempio

--- index.php
+++ index.php
@@ -1,11 +1,13 @@
 <?php
 include __DIR__ . '/models/product.php';
 
-$item_1 = new Product(1, 'Cane', 'Croccantini', 'https://arcaplanet.vtexassets.com/arquivos/ids/245173/almo-nature-holistic-cane-adult-medium-pollo-e-riso.jpg');
-$item_2 = new Product(2, 'Gatto', 'Scatoletta', 'https://arcaplanet.vtexassets.com/arquivos/ids/245336/almo-daily-menu-cat-400-gr-vitello.jpg');
+$item_1 = new Product(1, 'Cani', 'Croccantini', 'https://arcaplanet.vtexassets.com/arquivos/ids/245173/almo-nature-holistic-cane-adult-medium-pollo-e-riso.jpg');
+$item_2 = new Product(2, 'Gatti', 'Scatoletta', 'https://arcaplanet.vtexassets.com/arquivos/ids/245336/almo-daily-menu-cat-400-gr-vitello.jpg');
+$item_3 = new Product(3, 'Pesci', 'Mangime', 'https://arcaplanet.vtexassets.com/arquivos/ids/272714/tetra-guppy-mini-flakes.jpg');
+$item_4 = new Product(4, 'Uccelli', 'Voliera', 'https://arcaplanet.vtexassets.com/arquivos/ids/258384/voliera-wilma1.jpg');
 
 
-$products = [$item_1, $item_2];
+$products = [$item_1, $item_2, $item_3, $item_4];
 var_dump($products);
 ?>
 

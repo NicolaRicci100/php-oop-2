b4eb9727e6f84f51608c73f1993e63c81835cbc4
NicolaRicci100 <nicola.ricci09@gmail.com>
2023-07-25 17:06:49 +0200

    organizzo i prodotti in un array

--- index.php
+++ index.php
@@ -1,16 +1,5 @@
 <?php
 include_once __DIR__ . '/models/Product.php';
-
-$item_1 = new Product(1, 'Cani', 'Croccantini', 'https://arcaplanet.vtexassets.com/arquivos/ids/245173/almo-nature-holistic-cane-adult-medium-pollo-e-riso.jpg', 'cibo');
-$item_2 = new Product(2, 'Gatti', 'Scatoletta', 'https://arcaplanet.vtexassets.com/arquivos/ids/245336/almo-daily-menu-cat-400-gr-vitello.jpg', 'cibo');
-$item_3 = new Product(3, 'Pesci', 'Mangime', 'https://arcaplanet.vtexassets.com/arquivos/ids/272714/tetra-guppy-mini-flakes.jpg', 'cibo');
-$item_4 = new Product(4, 'Uccelli', 'Voliera', 'https://arcaplanet.vtexassets.com/arquivos/ids/258384/voliera-wilma1.jpg', 'casa');
-$item_5 = new Product(5, 'Gatti', 'Peluche', 'https://arcaplanet.vtexassets.com/arquivos/ids/223852/trixie-gatto-gioco-active-mouse-peluche.jpg', 'giocattolo');
-$item_6 = new Product(6, 'Pesci', 'Filtri', 'https://arcaplanet.vtexassets.com/arquivos/ids/272741/tetra-easycrystal-filterpack-250-300.jpg', 'casa');
-
-
-$products = [$item_1, $item_2, $item_3, $item_4, $item_5, $item_6];
-// var_dump($products);
 ?>
 
 
--- models/product.php
+++ models/product.php
@@ -16,3 +16,48 @@ class Product
     $this->type = $type;
   }
 }
+
+$products = [
+  new Product(
+    1,
+    'Cani',
+    'Croccantini',
+    'https://arcaplanet.vtexassets.com/arquivos/ids/245173/almo-nature-holistic-cane-adult-medium-pollo-e-riso.jpg',
+    'cibo'
+  ),
+  new Product(
+    2,
+    'Gatti',
+    'Scatoletta',
+    'https://arcaplanet.vtexassets.com/arquivos/ids/245336/almo-daily-menu-cat-400-gr-vitello.jpg',
+    'cibo'
+  ),
+  new Product(
+    3,
+    'Pesci',
+    'Mangime',
+    'https://arcaplanet.vtexassets.com/arquivos/ids/272714/tetra-guppy-mini-flakes.jpg',
+    'cibo'
+  ),
+  new Product(
+    4,
+    'Uccelli',
+    'Voliera',
+    'https://arcaplanet.vtexassets.com/arquivos/ids/258384/voliera-wilma1.jpg',
+    'casa'
+  ),
+  new Product(
+    5,
+    'Gatti',
+    'Peluche',
+    'https://arcaplanet.vtexassets.com/arquivos/ids/223852/trixie-gatto-gioco-active-mouse-peluche.jpg',
+    'giocattolo'
+  ),
+  new Product(
+    6,
+    'Pesci',
+    'Filtri',
+    'https://arcaplanet.vtexassets.com/arquivos/ids/272741/tetra-easycrystal-filterpack-250-300.jpg',
+    'casa'
+  )
+];
